d1fc586ebf2256b9c1bc12552dae606beb2054d3
Poy Chang <poypost@gmail.com>
2025-02-17 08:48:45 +0800

    update domain name from 'tscc.com.tw' to 'easycard.com.tw'

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # 自動填入台灣高鐵悠遊聯名卡搭乘資訊查詢系統
 
-如果你經常使用悠遊聯名卡搭乘台灣高鐵，並且要透過[台灣高鐵悠遊聯名卡搭乘資訊查詢系統](https://queryweb.tscc.com.tw/thsrc_web/)查詢搭查紀錄，以提供財務單位進行報帳，這個小工具能幫助你，自動填入你經常使用的悠遊卡號碼及使用者生日進行查詢。
+如果你經常使用悠遊聯名卡搭乘台灣高鐵，並且要透過[台灣高鐵悠遊聯名卡搭乘資訊查詢系統](https://queryweb.easycard.com.tw/thsrc_web/)查詢搭查紀錄，以提供財務單位進行報帳，這個小工具能幫助你，自動填入你經常使用的悠遊卡號碼及使用者生日進行查詢。
 
 ## 安裝方式
 
@@ -9,19 +9,19 @@
    * Internet Explorer 瀏覽器請點選主選單的 `[檢視]` / `[工具列]` / `[我的最愛列]` 進行切換。
 2. 將以下超連結 (**查詢高鐵搭乘資訊**)，直接利用滑鼠拖曳到**書籤工具列**或**我的最愛列**上
 
-   * [查詢高鐵搭乘資訊](https://queryweb.tscc.com.tw/thsrc_web/)
+   * [查詢高鐵搭乘資訊](https://queryweb.easycard.com.tw/thsrc_web/)
 
 3. 請你手動修改這個 **查詢高鐵搭乘資訊** 的超連結，並將下列程式碼中的 `YOUR_CARD_ID` (悠遊聯名卡卡號)和 `YOUR_BIRTHDAY` (持卡人生日)改成您的查詢資訊後，貼上網址欄為即可！
 
 ```js
-javascript:!function(){var t=location.href,e="https://queryweb.tscc.com.tw/thsrc_web/";t===e?(document.getElementById("txtCardID").value="YOUR_CARD_ID",document.getElementById("txtBirth").value="YOUR_BIRTHDAY"):"https://queryweb.tscc.com.tw/thsrc_web/THSRC_page.aspx"===t?(document.getElementById("VAT_Num").value="YOUR_VAT_NUMBER",document.getElementById("txtBuy").style.display="",document.getElementById("txtBuy").value="YOUR_VAT_NAME",document.getElementById("txtBank_code").value="YOUR_BANK_CODE"):location.href=e}();
+javascript:!function(){var t=location.href,e="https://queryweb.easycard.com.tw/thsrc_web/";t===e?(document.getElementById("txtCardID").value="YOUR_CARD_ID",document.getElementById("txtBirth").value="YOUR_BIRTHDAY"):"https://queryweb.easycard.com.tw/thsrc_web/THSRC_page.aspx"===t?(document.getElementById("VAT_Num").value="YOUR_VAT_NUMBER",document.getElementById("txtBuy").style.display="",document.getElementById("txtBuy").value="YOUR_VAT_NAME",document.getElementById("txtBank_code").value="YOUR_BANK_CODE"):location.href=e}();
 ```
 
 4. 若需要 **電子車票證明**，請再將修改程式碼中的 `YOUR_VAT_NUMBER` (公司統一編號)、`YOUR_VAT_NAME` (營利事業名稱)和 `YOUR_BANK_CODE` (悠遊聯名卡發卡銀行代號)改成您的查詢資訊後，貼上網址欄為即可！
 
 ## 使用方式
 
-1. 第一次點選書籤開啟[台灣高鐵悠遊聯名卡搭乘資訊查詢系統](https://queryweb.tscc.com.tw/thsrc_web/)
+1. 第一次點選書籤開啟[台灣高鐵悠遊聯名卡搭乘資訊查詢系統](https://queryweb.easycard.com.tw/thsrc_web/)
 2. 第二次點選書籤會輸入悠遊聯名卡卡號及持卡人生日
 
 部落格文章：[在台灣高鐵搭乘資訊查詢系統自動填入常用的悠遊卡號碼](https://poychang.github.io/autofill-tscc-boarding-info-query-paramter/)
--- src/tscc-query-autofill.js
+++ src/tscc-query-autofill.js
@@ -1,4 +1,16 @@
-(function() {
+(function () {
+    // 檢查是否已載入 Tesseract.js
+    if (!window.Tesseract) {
+        var script = document.createElement('script');
+        script.src = 'https://cdn.jsdelivr.net/npm/tesseract.js@4.0.2/dist/tesseract.min.js';
+        script.onload = function () {
+            runTesseract();
+        };
+        document.body.appendChild(script);
+    } else {
+        runTesseract();
+    }
+
     var ln = location.href;
     var cardID = 'YOUR_CARD_ID'; // 悠遊卡卡號
     var birth = 'YOUR_BIRTHDAY'; // 持卡人生日
@@ -6,14 +18,48 @@
     var vatName = 'YOUR_VAT_NAME'; // 營利事業名稱
     var bankCode = 'YOUR_BANK_CODE'; // 悠遊聯名卡發卡銀行代號
 
-    var tscc_url = 'https://queryweb.tscc.com.tw/thsrc_web/';
-    var tscc_purchase_proof_url = 'https://queryweb.tscc.com.tw/thsrc_web/THSRC_page.aspx';
+    var tscc_url = 'https://queryweb.easycard.com.tw/thsrc_web/';
+    var tscc_purchase_proof_url = 'https://queryweb.easycard.com.tw/thsrc_web/THSRC_page.aspx';
     if (ln === tscc_url) {
         document.getElementById('txtCardID').value = cardID;
         document.getElementById('txtBirth').value = birth;
+
+        const imgElement = document.getElementById('captchaImage');
+        try {
+            // 建立 canvas 元素來取得圖片的像素數據
+            const canvas = document.createElement('canvas');
+            const context = canvas.getContext('2d');
+
+            // 設置 canvas 尺寸與圖片相同
+            canvas.width = imgElement.width;
+            canvas.height = imgElement.height;
+
+            // 將圖像繪製到 canvas 上
+            context.drawImage(imgElement, 0, 0, imgElement.width, imgElement.height);
+
+            // 從 canvas 中獲取圖像數據
+            const dataUrl = canvas.toDataURL();
+
+            // 使用 Tesseract 進行識別
+            Tesseract.recognize(
+                dataUrl, // 使用 canvas 獲取的 base64 圖片數據
+                'eng', // 語言設置為英語（如果圖片包含中文字元，可以使用 'chi_tra'）
+                {
+                    logger: (info) => console.log(info), // 記錄進度
+                }
+            )
+                .then(({ data: { text } }) => {
+                    console.log(`辨識結果: ${text.trim()}`);
+                })
+                .catch((error) => {
+                    console.log(`辨識失敗: ${error.message}`);
+                });
+        } catch (error) {
+            console.log(`獲取圖片失敗: ${error.message}`);
+        }
     } else if (ln === tscc_purchase_proof_url) {
         document.getElementById('VAT_Num').value = vatNum;
-        document.getElementById('txtBuy').style.display='';
+        document.getElementById('txtBuy').style.display = '';
         document.getElementById('txtBuy').value = vatName;
         document.getElementById('txtBank_code').value = bankCode;
     } else {
